6cb5302968760c35ecc020b3cf3b26e915941605
taylorkraft <taylorkraft16@gmail.com>
2020-08-08 13:27:49 -0600

    render states

--- park.js
+++ park.js
@@ -1,7 +1,8 @@
 class Park {
-  constructor(id, name, location) {
+  constructor(id, name, location, state_id) {
     this.id = id
     this.name = name
     this.location = location
+    this.state_id = state_id
   }
 }
--- state.js
+++ state.js
@@ -4,4 +4,16 @@ class State {
     this.name = name
     this.flower = flower
   }
+
+  renderState() {
+    let statesDiv = document.getElementById("states-container")
+
+    statesDiv.innerHTML +=
+    `
+      
+        <label>Name: </label><h4>${this.name}</h4>
+        <label>State Flower: </label><h4>${this.flower}</h4>
+      
+    `
+  }
 }
