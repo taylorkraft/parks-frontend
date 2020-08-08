3e35a42393440f47ed4dee8c901c4eacf85eda04
taylorkraft <taylorkraft16@gmail.com>
2020-08-08 14:17:38 -0600

    notes

--- parkAdapter.js
+++ parkAdapter.js
@@ -4,10 +4,13 @@ class ParkAdapter {
 
   static fetchAndCreateParks() {
     return fetch(ParkAdaper.baseURL)
+    //fetch all parks
     .then(resp => resp.json())
+    //jsonify backend obj response
     .then(function(allParks) {
       return allParks.forEach(function(park) {
         return new Park(park)
+        //create new park instance for each park in allParks array
       })
     })
   }
