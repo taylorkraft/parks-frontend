037b1af5a10562ea34412412c597efa8c2373303
taylorkraft <taylorkraft16@gmail.com>
2020-08-08 13:54:32 -0600

    create elements

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  fetchStates()
 })
 
-function fetchStates() {
-  fetch("http://127.0.0.1:3000/states")
-  .then(resp => resp.json())
-  .then(states => {
-    for (const state of states) {
-      let s = new State(state.id, state.name, state.flower)
-      s.renderState()
-    }
-  })
-}
+// function fetchStates() {
+//   fetch("http://127.0.0.1:3000/states")
+//   .then(resp => resp.json())
+//   .then(states => {
+//     for (const state of states) {
+//       let s = new State(state.id, state.name, state.flower)
+//       s.renderState()
+//     }
+//   })
+// }
--- state.js
+++ state.js
@@ -1,19 +1,31 @@
 class State {
+
   constructor(id, name, flower) {
     this.id = id
     this.name = name
     this.flower = flower
+
+    this.main = document.createElement('div')
+    this.main.id = `state-${this.id}`
+    this.info = document.createElement('div')
+    this.info.id = `state-${this.id}-info`
+    this.parks = document.createElement('div')
+    this.parks.id = `state=${this.id}-parks`
+    this.edit = document.createElement('button')
+    this.edit.innerText = "Edit State Info"
+    this.main.append(this.info, this.parks, this.edit)
   }
 
-  renderState() {
-    let statesDiv = document.getElementById("states-container")
+  
+  // renderState() {
+  //   let statesDiv = document.getElementById("states-container")
 
-    statesDiv.innerHTML +=
-    `
+  //   statesDiv.innerHTML +=
+  //   `
       
-        <label>Name: </label><h4>${this.name}</h4>
-        <label>State Flower: </label><h4>${this.flower}</h4>
+  //       <label>Name: </label><h4>${this.name}</h4>
+  //       <label>State Flower: </label><h4>${this.flower}</h4>
       
-    `
-  }
+  //   `
+  // }
 }
